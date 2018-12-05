3eb5f3d893ee4d9d1303e858f5cd02094d0a230d
Dave Samojlenko <dsamojlenko@gmail.com>
2018-12-05 11:32:58 -0500

    update ip for rancher server

--- cds-snc.ca/rancher.cds-snc.ca.tf
+++ cds-snc.ca/rancher.cds-snc.ca.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "rancher-cds-snc-ca-A" {
     name    = "rancher.cds-snc.ca"
     type    = "A"
     records = [
-        "52.60.202.54"
+        "35.183.31.85"
     ]
     ttl     = "300"
 
