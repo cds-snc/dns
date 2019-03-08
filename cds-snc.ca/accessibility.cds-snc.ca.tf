95e1d30263b9d6cb3c7ae95004650aed76faf14f
Dave Samojlenko <dsamojlenko@gmail.com>
2019-03-08 09:42:38 -0500

    comment this out for now

--- cds-snc.ca/accessibility.cds-snc.ca.tf
+++ cds-snc.ca/accessibility.cds-snc.ca.tf
@@ -1,10 +1,10 @@
-resource "aws_route53_record" "accessibility-cds-snc-ca-CNAME" {
-    zone_id = "${aws_route53_zone.cds-snc-ca-public.zone_id}"
-    name    = "accessibility.cds-snc.ca"
-    type    = "CNAME"
-    records = [
-        "cds-snc.github.io"
-    ]
-    ttl     = "300"
-
-}
+#resource "aws_route53_record" "accessibility-cds-snc-ca-CNAME" {
+#    zone_id = "${aws_route53_zone.cds-snc-ca-public.zone_id}"
+#    name    = "accessibility.cds-snc.ca"
+#    type    = "CNAME"
+#    records = [
+#        "cds-snc.github.io"
+#    ]
+#    ttl     = "300"
+#
+#}
