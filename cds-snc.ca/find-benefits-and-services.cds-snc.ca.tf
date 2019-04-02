d9a40283b9f723345feea413972a5f1e5fa2c602
smcmurtry <smcmurtry@gmail.com>
2019-04-02 13:25:00 -0400

    added new subdomain for VAC repackaging work, points to heroku deployment

---
+++ cds-snc.ca/find-benefits-and-services.cds-snc.ca.tf
@@ -0,0 +1,10 @@
+resource "aws_route53_record" "find-benefits-and-services-cds-snc-ca-A" {
+    zone_id = "${aws_route53_zone.cds-snc-ca-public.zone_id}"
+    name    = "find-benefits-and-services.cds-snc.ca"
+    type    = "A"
+    records = [
+        "find-benefits-and-services.herokuapp.com"
+    ]
+    ttl     = "300"
+
+}
