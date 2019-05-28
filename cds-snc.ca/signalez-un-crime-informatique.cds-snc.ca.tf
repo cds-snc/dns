61efb4dd03f7cb40dafca65d2f3a8a0a0d5d7e78
Mike Williamson <mike.williamson@cds-snc.ca>
2019-05-28 16:28:31 -0400

    Fix invalid resource name
    Terraform hates dots.

--- cds-snc.ca/signalez-un-crime-informatique.cds-snc.ca.tf
+++ cds-snc.ca/signalez-un-crime-informatique.cds-snc.ca.tf
@@ -1,6 +1,6 @@
-resource "aws_route53_record" "report-a-cybercrime-cds-snc-ca-A" {
+resource "aws_route53_record" "signalez-un-crime-informatique-cds-snc-ca-A" {
     zone_id = "${aws_route53_zone.cds-snc-ca-public.zone_id}"
-    name    = "report-a-cybercrime.cds-snc.ca"
+    name    = "signalez-un-crime-informatique.cds-snc.ca"
     type    = "A"
     records = [
 			"40.85.218.68"
