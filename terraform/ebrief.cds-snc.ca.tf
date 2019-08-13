3c606a687cec14bd7ca89a49f3037f39f1e545b0
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-08-13 14:05:56 -0400

    update ebrief dns to new service (#40)

--- terraform/ebrief.cds-snc.ca.tf
+++ terraform/ebrief.cds-snc.ca.tf
@@ -1,9 +1,9 @@
 resource "aws_route53_record" "ebrief-cds-snc-ca-A" {
     zone_id = "${aws_route53_zone.cds-snc-ca-public.zone_id}"
-    name    = "ebrief.cds-snc.ca"
+    name    = "brief-bref.cds-snc.ca"
     type    = "A"
     records = [
-        "52.237.20.235"
+        "40.86.217.175"
     ]
     ttl     = "300"
 
