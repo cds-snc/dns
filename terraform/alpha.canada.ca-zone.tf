e80c30305fff2d4868981dc9ed4d1691b1acd99c
John O'Brien <obrien-j@users.noreply.github.com>
2019-08-29 09:27:56 -0400

    [ADD] CNAME for alpha.canada.ca landing page (#44)

--- terraform/alpha.canada.ca-zone.tf
+++ terraform/alpha.canada.ca-zone.tf
@@ -10,3 +10,14 @@ resource "aws_route53_zone" "alpha-canada-ca-public" {
 output "alpha-canada-ca-ns" {
   value="${aws_route53_zone.alpha-canada-ca-public.name_servers}"
 }
+
+resource "aws_route53_record" "alpha-canada-ca-CNAME" {
+    zone_id = "${aws_route53_zone.alpha-canada-ca-public.zone_id}"
+    name    = "alpha.canada.ca"
+    type    = "CNAME"
+    records = [
+        "alpha-canada-ca.github.io"
+    ]
+    ttl     = "300"
+
+}
