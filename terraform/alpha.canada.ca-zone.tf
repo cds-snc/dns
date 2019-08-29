10afe0aba7331aaee1c7814ee6821556e2cfc6b8
John O'Brien <obrien-j@users.noreply.github.com>
2019-08-29 15:08:27 -0400

    [UPDATE] Swap out CNAME for A record alpha.canada.ca (#45)
    Route53 doesn't support cname records on apex domains. Interestingly enough, terraform plan didn't fail on this.

--- terraform/alpha.canada.ca-zone.tf
+++ terraform/alpha.canada.ca-zone.tf
@@ -14,9 +14,12 @@ output "alpha-canada-ca-ns" {
 resource "aws_route53_record" "alpha-canada-ca-CNAME" {
     zone_id = "${aws_route53_zone.alpha-canada-ca-public.zone_id}"
     name    = "alpha.canada.ca"
-    type    = "CNAME"
+    type    = "A"
     records = [
-        "alpha-canada-ca.github.io"
+        "185.199.108.153",
+        "185.199.109.153",
+        "185.199.110.153",
+        "185.199.111.153"
     ]
     ttl     = "300"
 
