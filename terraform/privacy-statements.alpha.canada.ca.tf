73dedd8d06be6fc0dc9b2656a9ec86d32e79280f
Steve Astels <sastels@gmail.com>
2019-10-02 09:53:45 -0400

    change French url

--- terraform/privacy-statements.alpha.canada.ca.tf
+++ terraform/privacy-statements.alpha.canada.ca.tf
@@ -6,16 +6,14 @@ resource "aws_route53_record" "privacy-statements-cds-alpha-canada-ca-CNAME" {
         "simplify-privacy-statements-v2.herokuapp.com"
     ]
     ttl     = "300"
-
 }
 
-resource "aws_route53_record" "formulaires-confidentialite-snc-alpha-canada-ca-CNAME" {
+resource "aws_route53_record" "avis-confidentialite-snc-alpha-canada-ca-CNAME" {
     zone_id = "${aws_route53_zone.alpha-canada-ca-public.zone_id}"
-    name    = "formulaires-confidentialite.snc.alpha.canada.ca"
+    name    = "avis-confidentialite.snc.alpha.canada.ca"
     type    = "CNAME"
     records = [
         "simplify-privacy-statements-v2.herokuapp.com"
     ]
     ttl     = "300"
-    
 }
