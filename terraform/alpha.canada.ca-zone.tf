cfd85fadb8746cf70b4de973b511228e64651591
Max <max@neuvians.net>
2019-10-31 11:47:08 -0400

    Actually use ALIAS properly

--- terraform/alpha.canada.ca-zone.tf
+++ terraform/alpha.canada.ca-zone.tf
@@ -11,13 +11,16 @@ output "alpha-canada-ca-ns" {
   value="${aws_route53_zone.alpha-canada-ca-public.name_servers}"
 }
 
-resource "aws_route53_record" "alpha-canada-ca-ALIAS" {
+resource "aws_route53_record" "alpha-canada-ca-A" {
     zone_id = "${aws_route53_zone.alpha-canada-ca-public.zone_id}"
     name    = "alpha.canada.ca"
-    type    = "ALIAS"
-    records = [
-        "d2aopswi6c2sr6.cloudfront.net"
-    ]
+    type    = "A"
+    alias {
+        name                   = "d2aopswi6c2sr6.cloudfront.net"
+        zone_id                = "Z2FDTNDATAQYW2"
+        evaluate_target_health = true
+    }
+
     ttl     = "60"
 
 }
