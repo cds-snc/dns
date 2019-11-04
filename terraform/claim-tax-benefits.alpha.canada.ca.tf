d8617afcd3b7c910fbc6a77664c994b9a43ac3db
Charles Morin <charles.morin@cds-snc.ca>
2019-11-04 09:57:20 -0500

    renamed claim tax benefits file name and changed dns record to aws

---
+++ terraform/claim-tax-benefits.alpha.canada.ca.tf
@@ -0,0 +1,19 @@
+resource "aws_route53_record" "claim-tax-benefits-alpha-canada-ca-CNAME" {
+    zone_id = "${aws_route53_zone.alpha-canada-ca-public.zone_id}"
+    name    = "claim-tax-benefits.alpha.canada.ca"
+    type    = "CNAME"
+    records = [
+        "ctb-load-balancer-1283705966.ca-central-1.elb.amazonaws.com"
+    ]
+    ttl     = "300"
+}
+
+resource "aws_route53_record" "reclamer-des-avantages-fiscaux-alpha-canada-ca-CNAME" {
+    zone_id = "${aws_route53_zone.alpha-canada-ca-public.zone_id}"
+    name    = "reclamer-des-avantages-fiscaux.alpha.canada.ca"
+    type    = "CNAME"
+    records = [
+        "ctb-load-balancer-1283705966.ca-central-1.elb.amazonaws.com"
+    ]
+    ttl     = "300"
+}
