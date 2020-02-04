fd4c7f2d89ea36f95ff6a53fac48e4440f0b8162
Ryan Hyma <ryan.hyma@gmail.com>
2020-02-04 12:17:36 -0500

    Terraform entry to web templates experiment for PSPC

---
+++ terraform/web-templates.pspc.alpha.canada.ca.tf
@@ -0,0 +1,9 @@
+resource "aws_route53_record" "web-templates-pspc-alpha-canada-ca-A" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "web-templates.pspc.alpha.canada.ca"
+  type    = "A"
+  records = [
+    "52.237.15.42"
+  ]
+  ttl     = "300"
+}
