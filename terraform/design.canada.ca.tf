f63d73ceaeee415f2f3fd21d28bb2c2170e7627d
Max Neuvians <max@neuvians.net>
2020-02-10 13:01:18 -0500

    Rebase of #87

---
+++ terraform/design.canada.ca.tf
@@ -0,0 +1,19 @@
+resource "aws_route53_record" "design-alpha-canada-ca-A" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "design.alpha.canada.ca"
+  type    = "A"
+  records = [
+    "52.237.15.42"
+  ]
+  ttl     = "300"
+}
+
+resource "aws_route53_record" "conception-alpha-canada-ca-A" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "conception.alpha.canada.ca"
+  type    = "A"
+  records = [
+    "52.237.15.42"
+  ]
+  ttl     = "300"
+}
