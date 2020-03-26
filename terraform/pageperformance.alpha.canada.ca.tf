43795a41a773b9e27d884963b1eebd0133410fbc
Ryan Hyma <ryan.hyma@gmail.com>
2020-03-26 09:05:57 -0400

    Removed cer from record

--- terraform/pageperformance.alpha.canada.ca.tf
+++ terraform/pageperformance.alpha.canada.ca.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_record" "pageperformance-cer-alpha-canada-ca-A" {
+resource "aws_route53_record" "pageperformance-alpha-canada-ca-A" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "pageperformance.alpha.canada.ca"
   type    = "A"
