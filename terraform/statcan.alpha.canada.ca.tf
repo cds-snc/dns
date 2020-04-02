9db2d73a852fe00c974c5eaddd9b3b5020f03a67
Max Neuvians <max@neuvians.net>
2020-04-02 14:31:16 -0400

    Fixed on comments

--- terraform/statcan.alpha.canada.ca.tf
+++ terraform/statcan.alpha.canada.ca.tf
@@ -30,7 +30,7 @@ resource "aws_route53_record" "energy-information-alpha-canada-ca-CNAME" {
 
 resource "aws_route53_record" "information-energie-alpha-canada-ca-CNAME" {
     zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-    name    = "statistique.alpha.canada.ca"
+    name    = " information-energie.alpha.canada.ca"
     type    = "CNAME"
     records = [
         "www-alpha.prod.cloud.statcan.ca"
