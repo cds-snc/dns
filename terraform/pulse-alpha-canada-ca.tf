373e54f0f790d39777ba1ce53a17f67ac6f02139
John O'Brien <obrien-j@users.noreply.github.com>
2020-05-15 11:48:05 -0400

    Swap in actual IP / A record

--- terraform/pulse-alpha-canada-ca.tf
+++ terraform/pulse-alpha-canada-ca.tf
@@ -1,19 +1,19 @@
-resource "aws_route53_record" "pulse-alpha-canada-ca-CNAME" {
+resource "aws_route53_record" "pulse-alpha-canada-ca-A" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "pulse.alpha.canada.ca"
-  type    = "CNAME"
+  type    = "A"
   records = [
-    "digital.canada.ca"
+    "34.95.5.243"
   ]
   ttl     = "300"
 }
 
-resource "aws_route53_record" "pouls-alpha-canada-ca-CNAME" {
+resource "aws_route53_record" "pouls-alpha-canada-ca-A" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "pouls.alpha.canada.ca"
-  type    = "CNAME"
+  type    = "A"
   records = [
-    "numerique.canada.ca"
+    "34.95.5.243"
   ]
   ttl     = "300"
 }
