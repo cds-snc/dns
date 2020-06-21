e86cc68667efb2d1ed3181e740362e8f1ef558f9
Max Neuvians <max@neuvians.net>
2020-06-21 16:44:17 -0400

    Remove periods

--- terraform/covid-notification.alpha.canada.ca.tf
+++ terraform/covid-notification.alpha.canada.ca.tf
@@ -3,10 +3,10 @@ resource "aws_route53_record" "covid-notification-alpha-canada-ca-NS" {
     name    = "covid-notification.alpha.canada.ca"
     type    = "NS"
     records = [
-        "ns-1714.awsdns-22.co.uk.",
-        "ns-93.awsdns-11.com.",
-        "ns-1334.awsdns-38.org.",
-        "ns-842.awsdns-41.net."
+        "ns-1714.awsdns-22.co.uk",
+        "ns-93.awsdns-11.com",
+        "ns-1334.awsdns-38.org",
+        "ns-842.awsdns-41.net"
     ]
     ttl     = "300"
 }
