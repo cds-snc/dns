f580039ec4b899816d51c755bedb0b3f754ed5b6
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-18 14:39:44 -0500

    feat: add DKIM, SPF and DMARC for notification.canada.ca

--- terraform/notification.canada.ca-zone.tf
+++ terraform/notification.canada.ca-zone.tf
@@ -11,7 +11,7 @@ output "notification-canada-ca-ns" {
   value= "${aws_route53_zone.notification-canada-ca-public.name_servers}"
 }
 
-resource "aws_route53_record" "pbmm-notification-canada-ca-ACM-cname" {
+resource "aws_route53_record" "notification-canada-ca-ACM-cname" {
     zone_id = aws_route53_zone.notification-canada-ca-public.zone_id
     name    = "_2115a5004ab7895234c60254e152046b.notification.canada.ca"
     type    = "CNAME"
@@ -21,12 +21,62 @@ resource "aws_route53_record" "pbmm-notification-canada-ca-ACM-cname" {
     ttl     = "300"
 }
 
-resource "aws_route53_record" "pbmm-document-notification-canada-ca-ACM-cname" {
+resource "aws_route53_record" "document-notification-canada-ca-ACM-cname" {
     zone_id = aws_route53_zone.notification-canada-ca-public.zone_id
     name    = "_db43d1cf891afd4671fb913d18ef0a0e.document.notification.canada.ca"
     type    = "CNAME"
     records = [
         "_130ea19fa1fdd9e59b7632fbac0d7e00.wggjkglgrm.acm-validations.aws."
+    ]
+    ttl     = "300"
+}
+
+resource "aws_route53_record" "dkim1-notification-canada-ca-CNAME" {
+    zone_id = aws_route53_zone.notification-canada-ca-public.zone_id
+    name    = "wrs6wsp65k764hnaouax5t66vfqrbrst._domainkey.notification.canada.ca"
+    type    = "CNAME"
+    records = [
+        "wrs6wsp65k764hnaouax5t66vfqrbrst.dkim.amazonses.com"
+    ]
+    ttl     = "300"
+}
+
+resource "aws_route53_record" "dkim2-notification-canada-ca-CNAME" {
+    zone_id = aws_route53_zone.notification-canada-ca-public.zone_id
+    name    = "wrtaqi2wdu42zqjzyf3ikn46kzos4f76._domainkey.notification.canada.ca"
+    type    = "CNAME"
+    records = [
+        "wrtaqi2wdu42zqjzyf3ikn46kzos4f76.dkim.amazonses.com"
+    ]
+    ttl     = "300"
+}
+
+resource "aws_route53_record" "dkim3-notification-canada-ca-CNAME" {
+    zone_id = aws_route53_zone.notification-canada-ca-public.zone_id
+    name    = "h2d5mnabqwlnowww7rkgpoagtrxt7d4z._domainkey.notification.canada.ca"
+    type    = "CNAME"
+    records = [
+        "h2d5mnabqwlnowww7rkgpoagtrxt7d4z.dkim.amazonses.com"
+    ]
+    ttl     = "300"
+}
+
+resource "aws_route53_record" "notification-canada-ca-SPF" {
+    zone_id = aws_route53_zone.notification-canada-ca-public.zone_id
+    name    = "notification.alpha.canada.ca"
+    type    = "TXT"
+    records = [
+        "v=spf1 include:amazonses.com ~all"
+    ]
+    ttl     = "300"
+}
+
+resource "aws_route53_record" "notification-canada-ca-DMARC" {
+    zone_id = aws_route53_zone.notification-canada-ca-public.zone_id
+    name    = "_dmarc.notification.alpha.canada.ca"
+    type    = "TXT"
+    records = [
+        "v=DMARC1; p=reject; sp=reject; pct=100; rua=mailto:dmarc@cyber.gc.ca"
     ]
     ttl     = "300"
 }
