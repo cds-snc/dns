eb9eb03f71d716193840ebcb847410b295bcc9d7
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-18 14:28:42 -0500

    Move records

--- terraform/notification.alpha.canada.ca.tf
+++ terraform/notification.alpha.canada.ca.tf
@@ -189,23 +189,3 @@ resource "aws_route53_record" "pbmm-document-notification-alpha-canada-ca-ACM-cn
     ]
     ttl     = "300"
 }
-
-resource "aws_route53_record" "pbmm-notification-canada-ca-ACM-cname" {
-    zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-    name    = "_2115a5004ab7895234c60254e152046b.notification.canada.ca"
-    type    = "CNAME"
-    records = [
-        "_aaacd89cd470de0970c70c7ab1b7d4d5.wggjkglgrm.acm-validations.aws."
-    ]
-    ttl     = "300"
-}
-
-resource "aws_route53_record" "pbmm-document-notification-canada-ca-ACM-cname" {
-    zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-    name    = "_db43d1cf891afd4671fb913d18ef0a0e.document.notification.canada.ca"
-    type    = "CNAME"
-    records = [
-        "_130ea19fa1fdd9e59b7632fbac0d7e00.wggjkglgrm.acm-validations.aws."
-    ]
-    ttl     = "300"
-}
