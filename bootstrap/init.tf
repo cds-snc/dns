d82d67b1d5491d114d4dcc60ab9ee3dc19edb1cb
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-01-28 15:00:22 -0500

    Upgrade Terraform to 0.13

--- bootstrap/init.tf
+++ bootstrap/init.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
 provider "aws" {
   region  = "us-east-1"
 }
--- terraform/init.tf
+++ terraform/init.tf
@@ -1,6 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
 provider "aws" {
   region  = "us-east-1"
-  version = "~> 2.70"
 }
 
 terraform {
