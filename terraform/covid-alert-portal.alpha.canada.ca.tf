20e9be7ddbb3d950523e1121743c5e8f42b58ea1
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-05-06 12:17:49 -0400

    feat: add COVID Alert business registration site URLs

--- terraform/covid-alert-portal.alpha.canada.ca.tf
+++ terraform/covid-alert-portal.alpha.canada.ca.tf
@@ -19,4 +19,27 @@ resource "aws_route53_record" "portail-alerte-covid-alpha-canada-ca-CNAME" {
     "covid-alert-portal.alpha.canada.ca"
   ]
   ttl = "300"
+}
+
+resource "aws_route53_record" "covid-alert-portal-alpha-canada-ca-NS" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "covid-alert-qr-poster.alpha.canada.ca"
+  type    = "NS"
+  records = [
+    "ns-905.awsdns-49.net",
+    "ns-29.awsdns-03.com",
+    "ns-2045.awsdns-63.co.uk",
+    "ns-1156.awsdns-16.org"
+  ]
+  ttl = "300"
+}
+
+resource "aws_route53_record" "portail-alerte-covid-alpha-canada-ca-CNAME" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "alerte-covid-affiche-qr.alpha.canada.ca"
+  type    = "CNAME"
+  records = [
+    "covid-alert-qr-poster.alpha.canada.ca"
+  ]
+  ttl = "300"
 } 
