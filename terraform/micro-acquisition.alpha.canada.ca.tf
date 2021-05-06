d067be50d85f76e2977e92c416f24062f876e839
sebz <smellems@gmail.com>
2021-05-06 08:14:46 -0400

    Change zone id and name

--- terraform/micro-acquisition.alpha.canada.ca.tf
+++ terraform/micro-acquisition.alpha.canada.ca.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "micro-acquisition-alpha-canada-ca-CNAME" {
-    zone_id = aws_route53_zone.cds-snc-ca-public.zone_id
-    name    = "micro-acquisition-alpha-canada-ca-CNAME"
+    zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+    name    = "micro-acquisition.alpha.canada.ca"
     type    = "CNAME"
     records = [
         "canada-ca.github.io"
