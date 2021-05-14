b4349bea8d6d4e2a2c05ee26f98e513875b1bdc3
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-05-14 10:02:13 -0400

    Create record for micro-acquisition.alpha.canada.ca

---
+++ terraform/micro-acquisition.alpha.canada.ca.tf
@@ -0,0 +1,9 @@
+resource "aws_route53_record" "micro-acquisition-alpha-canada-ca-CNAME" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "micro-acquisition.alpha.canada.ca"
+  type    = "CNAME"
+  records = [
+    "canada-ca.github.io"
+  ]
+  ttl = "300"
+}
