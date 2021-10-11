081b7a9f1993649d90a49b3dddcd642adbf9eb7c
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-10-11 12:29:07 -0400

    feat: revert A rec to cname for workplace

--- terraform/workplace.alpha.canada.ca.tf
+++ terraform/workplace.alpha.canada.ca.tf
@@ -1,44 +1,21 @@
 
-# resource "aws_route53_record" "workplace-alpha-canada-ca-a" {
-#   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-#   name    = "workplace.alpha.canada.ca"
-#   type    = "A"
-#   records = [
-#     "20.104.249.101"
-#   ]
-#   ttl = "300"
-
-# }
-
-# resource "aws_route53_record" "milieu-travail-alpha-canada-ca-A" {
-#   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-#   name    = "milieu-travail.alpha.canada.ca"
-#   type    = "A"
-#   records = [
-#     "20.104.249.101"
-#   ]
-#   ttl = "300"
-
-# }
-
-resource "aws_route53_record" "workplace-alpha-canada-ca-CNAME" {
+resource "aws_route53_record" "workplace-alpha-canada-ca-a" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "workplace.alpha.canada.ca"
-  type    = "CNAME"
+  type    = "A"
   records = [
-    "gcr2wp-dev.powerappsportals.com"
+    "20.104.249.101"
   ]
   ttl = "300"
-
 }
 
-resource "aws_route53_record" "milieu-travail-alpha-canada-ca-CNAME" {
+resource "aws_route53_record" "milieu-travail-alpha-canada-ca-A" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "milieu-travail.alpha.canada.ca"
-  type    = "CNAME"
+  type    = "A"
   records = [
-    "gcr2wp-dev.powerappsportals.com"
+    "20.104.249.101"
   ]
   ttl = "300"
-
 }
+
