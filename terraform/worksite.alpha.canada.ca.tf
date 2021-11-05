9c0454cf21b1c16ff5ff0e168df7db9028d0dd56
Max Neuvians <max.neuvians@cds-snc.ca>
2021-11-05 16:42:22 -0400

    fix: temp adds CNAME for worksite

--- terraform/worksite.alpha.canada.ca.tf
+++ terraform/worksite.alpha.canada.ca.tf
@@ -1,10 +1,20 @@
 
+//resource "aws_route53_record" "worksite-alpha-canada-ca-a" {
+//  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+//  name    = "worksite.alpha.canada.ca"
+//  type    = "A"
+//  records = [
+//    "20.104.121.246"
+//  ]
+//  ttl = "300"
+//}
+
 resource "aws_route53_record" "worksite-alpha-canada-ca-a" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "worksite.alpha.canada.ca"
-  type    = "A"
+  type    = "CNAME"
   records = [
-    "20.104.121.246"
+    "gcr2wp-non-cpa-dev.powerappsportals.com"
   ]
   ttl = "300"
 }
