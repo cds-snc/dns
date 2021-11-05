84bb44c52d2a98fc3e712bc8540e8737094750e5
Max Neuvians <max.neuvians@cds-snc.ca>
2021-11-05 16:49:03 -0400

    fix: reverts to A record

--- terraform/worksite.alpha.canada.ca.tf
+++ terraform/worksite.alpha.canada.ca.tf
@@ -1,20 +1,10 @@
 
-//resource "aws_route53_record" "worksite-alpha-canada-ca-a" {
-//  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-//  name    = "worksite.alpha.canada.ca"
-//  type    = "A"
-//  records = [
-//    "20.104.121.246"
-//  ]
-//  ttl = "300"
-//}
-
 resource "aws_route53_record" "worksite-alpha-canada-ca-a" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "worksite.alpha.canada.ca"
-  type    = "CNAME"
+  type    = "A"
   records = [
-    "gcr2wp-non-cpa-dev.powerappsportals.com"
+    "20.104.121.246"
   ]
   ttl = "300"
 }
