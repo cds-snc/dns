d7aba0de3a27ead47276f1ea7fdd957f359911f5
Mike Williamson <mike@korora.ca>
2022-03-17 13:12:58 -0400

    Delegate to the tracker ns
    This commit switches from using cnames to actually delegating to the
    Tracker alpha nameserver.

--- terraform/tracker-alpha-canada-ca.tf
+++ terraform/tracker-alpha-canada-ca.tf
@@ -1,40 +1,25 @@
-resource "aws_route53_record" "tracker-alpha-canada-ca-A" {
+resource "aws_route53_record" "tracker-alpha-canada-ca-NS" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "tracker.alpha.canada.ca"
-  type    = "A"
-  records = [
-    "34.95.5.243"
-  ]
-  ttl = "300"
-}
-
-resource "aws_route53_record" "suivi-alpha-canada-ca-A" {
-  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-  name    = "suivi.alpha.canada.ca"
-  type    = "A"
-  records = [
-    "34.95.5.243"
-  ]
-  ttl = "300"
-}
-
-resource "aws_route53_record" "log4shell-tracker-alpha-canada-ca-NS" {
-  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-  name    = "log4shell.tracker.alpha.canada.ca"
   type    = "NS"
   records = [
-    "ns1-log4shell.tracker.alpha.canada.ca",
+    "ns-cloud-a1.googledomains.com.",
+    "ns-cloud-a2.googledomains.com.",
+    "ns-cloud-a3.googledomains.com.",
+    "ns-cloud-a4.googledomains.com.",
   ]
   ttl = "300"
 }
 
-resource "aws_route53_record" "ns1-log4shell-tracker-alpha-canada-ca-A" {
+resource "aws_route53_record" "suivi-alpha-canada-ca-NS" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-  name    = "ns1-log4shell.tracker.alpha.canada.ca"
-  type    = "A"
+  name    = "suivi.alpha.canada.ca"
+  type    = "NS"
   records = [
-    "34.95.53.80"
+    "ns-cloud-a1.googledomains.com.",
+    "ns-cloud-a2.googledomains.com.",
+    "ns-cloud-a3.googledomains.com.",
+    "ns-cloud-a4.googledomains.com.",
   ]
   ttl = "300"
 }
-
