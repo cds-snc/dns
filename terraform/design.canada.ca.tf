24c08de8bc1debc5e269233950aac0434639b92c
Ethan Wallace <ethan.wallace.91@gmail.com>
2022-04-27 07:16:57 -0400

    Add TXT record to verify domain

--- terraform/design.canada.ca.tf
+++ terraform/design.canada.ca.tf
@@ -7,3 +7,13 @@ resource "aws_route53_record" "design-alpha-canada-ca-A" {
   ]
   ttl = "300"
 }
+
+resource "aws_route53_record" "design-alpha-canada-ca-A" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "design.alpha.canada.ca"
+  type    = "TXT"
+  records = [
+    "91447"
+  ]
+  ttl = "300"
+}
