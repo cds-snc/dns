90188cf6bcb16d56d8bea3672b698ea6d7badd34
Ethan Wallace <ethan.wallace.91@gmail.com>
2022-04-27 13:53:53 -0400

    Add TXT record to verify domain (#245)
    * Add TXT record to verify domain
    
    * Update design.canada.ca.tf
    
    * Update design.canada.ca.tf

--- terraform/design.canada.ca.tf
+++ terraform/design.canada.ca.tf
@@ -1,9 +1,19 @@
-resource "aws_route53_record" "design-alpha-canada-ca-A" {
+resource "aws_route53_record" "design-alpha-canada-ca-CNAME" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "design.alpha.canada.ca"
   type    = "CNAME"
   records = [
     "gallant-bassi-851d24.netlify.app"
+  ]
+  ttl = "300"
+}
+
+resource "aws_route53_record" "design-alpha-canada-ca-TXT" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "design.alpha.canada.ca"
+  type    = "TXT"
+  records = [
+    "91447"
   ]
   ttl = "300"
 }
