9aeb25631f9eefdcec93541d1fb0fb73fee3423d
Ethan Wallace <ethan.wallace.91@gmail.com>
2022-04-29 09:35:04 -0400

    Update design.canada.ca.tf

--- terraform/design.canada.ca.tf
+++ terraform/design.canada.ca.tf
@@ -1,19 +1,9 @@
-resource "aws_route53_record" "design-alpha-canada-ca-A" {
+resource "aws_route53_record" "design-alpha-canada-ca-CNAME" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "design.alpha.canada.ca"
-  type    = "A"
+  type    = "CNAME"
   records = [
-    "52.237.15.42"
-  ]
-  ttl = "300"
-}
-
-resource "aws_route53_record" "design-alpha-canada-ca-TXT" {
-  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-  name    = "design.alpha.canada.ca"
-  type    = "TXT"
-  records = [
-    "91447"
+    "gallant-bassi-851d24.netlify.app"
   ]
   ttl = "300"
 }
