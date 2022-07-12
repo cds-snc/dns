aa76d8a1b3728920771bad1ec2efc4f53b7ec878
Mike Williamson <mike@korora.ca>
2022-07-12 18:15:29 -0400

    Delegate safeinputs dns
    Safeinputs is an innovation project at PHAC. As the name implies it's
    aiming to safely handle inputs and that safety starts with encryption,
    which requires a domain name... hence this PR.

---
+++ terraform/safeinputs-alpha-canada-ca.tf
@@ -0,0 +1,25 @@
+resource "aws_route53_record" "tracker-alpha-canada-ca-NS" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "safeinputs.alpha.canada.ca"
+  type    = "NS"
+  records = [
+    "ns-cloud-e1.googledomains.com.",
+    "ns-cloud-e2.googledomains.com.",
+    "ns-cloud-e3.googledomains.com.",
+    "ns-cloud-e4.googledomains.com.",
+  ]
+  ttl = "300"
+}
+
+resource "aws_route53_record" "suivi-alpha-canada-ca-NS" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "entreessecurisees.alpha.canada.ca"
+  type    = "NS"
+  records = [
+    "ns-cloud-e1.googledomains.com.",
+    "ns-cloud-e2.googledomains.com.",
+    "ns-cloud-e3.googledomains.com.",
+    "ns-cloud-e4.googledomains.com.",
+  ]
+  ttl = "300"
+}
