227d4cbf0a38d5c9bc53e44eaa5c1836024cf1a4
Calvin Rodo <calvin.rodo@cds-snc.ca>
2022-09-22 16:33:01 -0400

    feat: adding ITAP PR Changes. (#271)
    * feat: adding ITAP PR Changes.
    
    Because we stil use Github Secrets a CDS employee needs to create this
    PR.
    
    Closes https://github.com/cds-snc/dns/pull/267
    
    * fix: update name to correct sub-domain

---
+++ terraform/itap-piem.alpha-canada.ca.tf
@@ -0,0 +1,9 @@
+resource "aws_route53_record" "itap-piem-alpha-canada-ca-A" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "itap-piem.alpha.canada.ca"
+  type    = "A"
+  records = [
+    "34.111.247.109"
+  ]
+  ttl = "300"
+}
