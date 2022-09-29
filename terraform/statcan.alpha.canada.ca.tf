a2427e65a4bcb663cea1ca8fcbe23c3388209fb6
Calvin Rodo <calvin.rodo@cds-snc.ca>
2022-09-29 12:25:38 -0400

    feat: copy over from statcan pr (#273)

--- terraform/statcan.alpha.canada.ca.tf
+++ terraform/statcan.alpha.canada.ca.tf
@@ -1,9 +1,24 @@
+locals {
+  acme_challenge       = "_acme_challenge"
+  challenges_subdomain = "challenges.cloud.statcan.ca"
+}
+
 resource "aws_route53_record" "statistics-alpha-canada-ca-CNAME" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "statistics.alpha.canada.ca"
   type    = "CNAME"
   records = [
     "www-alpha.prod.cloud.statcan.ca"
+  ]
+  ttl = "300"
+}
+
+resource "aws_route53_record" "_acme-challenge-statistics-alpha-canada-ca-CNAME" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = join(".", [local.acme_challenge, aws_route53_record.statistics-alpha-canada-ca-CNAME.name])
+  type    = "CNAME"
+  records = [
+    join(".", [aws_route53_record.statistics-alpha-canada-ca-CNAME.name, local.challenges_subdomain])
   ]
   ttl = "300"
 }
@@ -18,6 +33,16 @@ resource "aws_route53_record" "statistique-alpha-canada-ca-CNAME" {
   ttl = "300"
 }
 
+resource "aws_route53_record" "_acme-challenge-statistique-alpha-canada-ca-CNAME" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = join(".", [local.acme_challenge, aws_route53_record.statistique-alpha-canada-ca-CNAME.name])
+  type    = "CNAME"
+  records = [
+    join(".", [aws_route53_record.statistique-alpha-canada-ca-CNAME.name, local.challenges_subdomain])
+  ]
+  ttl = "300"
+}
+
 resource "aws_route53_record" "energy-information-alpha-canada-ca-CNAME" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "energy-information.alpha.canada.ca"
@@ -28,12 +53,32 @@ resource "aws_route53_record" "energy-information-alpha-canada-ca-CNAME" {
   ttl = "300"
 }
 
+resource "aws_route53_record" "_acme-challenge-energy-information-alpha-canada-ca-CNAME" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = join(".", [local.acme_challenge, aws_route53_record.energy-information-alpha-canada-ca-CNAME.name])
+  type    = "CNAME"
+  records = [
+    join(".", [aws_route53_record.energy-information-alpha-canada-ca-CNAME.name, local.challenges_subdomain])
+  ]
+  ttl = "300"
+}
+
 resource "aws_route53_record" "information-energie-alpha-canada-ca-CNAME" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "information-energie.alpha.canada.ca"
   type    = "CNAME"
   records = [
     "ccei-alpha.prod.cloud.statcan.ca"
+  ]
+  ttl = "300"
+}
+
+resource "aws_route53_record" "_acme-challenge-information-energie-alpha-canada-ca-CNAME" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = join(".", [local.acme_challenge, aws_route53_record.information-energie-alpha-canada-ca-CNAME.name])
+  type    = "CNAME"
+  records = [
+    join(".", [aws_route53_record.information-energie-alpha-canada-ca-CNAME.name, local.challenges_subdomain])
   ]
   ttl = "300"
 }
