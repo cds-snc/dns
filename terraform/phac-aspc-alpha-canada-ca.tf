420d081f01546fe9d27e80a601bb500f33bb7df4
Keith <youngka@gmail.com>
2023-03-18 07:57:42 -0400

    Create phac-aspc-alpha-canada-ca.tf
    New subdomains for delegation for PHAC

---
+++ terraform/phac-aspc-alpha-canada-ca.tf
@@ -0,0 +1,38 @@
+resource "aws_route53_record" "phac-aspc-alpha-canada-ca-NS" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "phac-aspc.alpha.canada.ca"
+  type    = "NS"
+  records = [
+    "ns-cloud-c1.googledomains.com.",
+    "ns-cloud-c2.googledomains.com.",
+    "ns-cloud-c3.googledomains.com.",
+    "ns-cloud-c4.googledomains.com.",
+  ]
+  ttl = "300"
+}
+
+resource "aws_route53_record" "phac-alpha-canada-ca-NS" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "phac.alpha.canada.ca"
+  type    = "NS"
+  records = [
+    "ns-cloud-d1.googledomains.com.",
+    "ns-cloud-d2.googledomains.com.",
+    "ns-cloud-d3.googledomains.com.",
+    "ns-cloud-d4.googledomains.com.",
+  ]
+  ttl = "300"
+}
+
+resource "aws_route53_record" "aspc-alpha-canada-ca-NS" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "aspc.alpha.canada.ca"
+  type    = "NS"
+  records = [
+    "ns-cloud-d1.googledomains.com.",
+    "ns-cloud-d2.googledomains.com.",
+    "ns-cloud-d3.googledomains.com.",
+    "ns-cloud-d4.googledomains.com.",
+  ]
+  ttl = "300"
+}
