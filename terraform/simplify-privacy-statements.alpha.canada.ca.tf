a7c841caf1c5612a90a2746a7aa2af07cb52e18a
omartehsin1 <omartehsin@gmail.com>
2023-03-30 13:41:54 -0400

    added old CNAME resources

--- terraform/simplify-privacy-statements.alpha.canada.ca.tf
+++ terraform/simplify-privacy-statements.alpha.canada.ca.tf
@@ -10,3 +10,23 @@ resource "aws_route53_record" "simplify-privacy-statements-alpha-canada-ca-NS" {
   ]
   ttl = "300"
 }
+
+resource "aws_route53_record" "privacy-statements-cds-alpha-canada-ca-CNAME" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "privacy-statements.cds.alpha.canada.ca"
+  type    = "CNAME"
+  records = [
+    "radiant-damselfly-fxzk8vc81qdj2jzmuog6ncda.herokudns.com"
+  ]
+  ttl = "300"
+}
+
+resource "aws_route53_record" "avis-confidentialite-snc-alpha-canada-ca-CNAME" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "avis-confidentialite.snc.alpha.canada.ca"
+  type    = "CNAME"
+  records = [
+    "behavioural-scallop-nlrxwf5edg8eivh9qeu0ai9p.herokudns.com"
+  ]
+  ttl = "300"
+}
