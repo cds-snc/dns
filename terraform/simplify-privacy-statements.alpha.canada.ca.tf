ef4e597afc2bbe7e6e5be03247ffa10460f9b848
Omar Tehsin <33768337+omartehsin1@users.noreply.github.com>
2023-03-30 13:46:32 -0400

    update/add-subdomain-delegation-to-hz (#322)
    * removed CNAME, added NS record
    
    * updated file name
    
    * added old CNAME resources

--- terraform/simplify-privacy-statements.alpha.canada.ca.tf
+++ terraform/simplify-privacy-statements.alpha.canada.ca.tf
@@ -1,3 +1,16 @@
+resource "aws_route53_record" "simplify-privacy-statements-alpha-canada-ca-NS" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "simplify-privacy-statements.alpha.canada.ca"
+  type    = "NS"
+  records = [
+    "ns-869.awsdns-44.net",
+    "ns-161.awsdns-20.com",
+    "ns-1385.awsdns-45.org",
+    "ns-1862.awsdns-40.co.uk"
+  ]
+  ttl = "300"
+}
+
 resource "aws_route53_record" "privacy-statements-cds-alpha-canada-ca-CNAME" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "privacy-statements.cds.alpha.canada.ca"
