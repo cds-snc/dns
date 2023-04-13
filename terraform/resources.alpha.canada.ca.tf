7bac985479759c8eb1bee28df776e7a345d84628
Daine Trinidad <daine.trinidad@gmail.com>
2023-04-13 13:44:05 -0700

    Update learning resources domains (#324)
    Point the domains to GH Pages

--- terraform/resources.alpha.canada.ca.tf
+++ terraform/resources.alpha.canada.ca.tf
@@ -1,25 +1,19 @@
-resource "aws_route53_record" "resources-alpha-canada-ca-NS" {
+resource "aws_route53_record" "resources-alpha-canada-ca-CNAME" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "resources.alpha.canada.ca"
-  type    = "NS"
+  type    = "CNAME"
   records = [
-    "ns-419.awsdns-52.com",
-    "ns-663.awsdns-18.net",
-    "ns-1655.awsdns-14.co.uk",
-    "ns-1128.awsdns-13.org"
+    "cds-snc.github.io"
   ]
-  ttl = "300"
+  ttl = "1800"
 }
 
-resource "aws_route53_record" "ressources-alpha-canada-ca-NS" {
+resource "aws_route53_record" "ressources-alpha-canada-ca-CNAME" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "ressources.alpha.canada.ca"
-  type    = "NS"
+  type    = "CNAME"
   records = [
-    "ns-648.awsdns-17.net",
-    "ns-1251.awsdns-28.org",
-    "ns-101.awsdns-12.com",
-    "ns-1955.awsdns-52.co.uk"
+    "cds-snc.github.io"
   ]
-  ttl = "300"
+  ttl = "1800"
 }
