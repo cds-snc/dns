065c5c414b72524a5c8bb05a8924cef768d254e4
Daine Trinidad <daine.trinidad@cds-snc.ca>
2023-12-14 14:09:07 -0800

    fix: update NS records for design system

--- terraform/design-system.alpha.canada.ca.tf
+++ terraform/design-system.alpha.canada.ca.tf
@@ -1,32 +1,25 @@
-resource "aws_route53_record" "cdn-design-system-alpha-canada-ca-NS" {
-  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-  name    = "cdn.design-system.alpha.canada.ca"
-  type    = "NS"
-  records = [
-    "ns-1114.awsdns-11.org",
-    "ns-830.awsdns-39.net",
-    "ns-2010.awsdns-59.co.uk",
-    "ns-111.awsdns-13.com"
-  ]
-  ttl = "300"
-}
-
-resource "aws_route53_record" "design-system-alpha-canada-ca-CNAME" {
+resource "aws_route53_record" "design-system-alpha-canada-ca-NS" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "design-system.alpha.canada.ca"
-  type    = "CNAME"
+  type    = "NS"
   records = [
-    "cds-design-snc.netlify.app"
+    "ns-1553.awsdns-02.co.uk",
+    "ns-527.awsdns-01.net",
+    "ns-1282.awsdns-32.org",
+    "ns-299.awsdns-37.com"
   ]
   ttl = "300"
 }
 
-resource "aws_route53_record" "systeme-design-alpha-canada-ca-CNAME" {
+resource "aws_route53_record" "systeme-design-alpha-canada-ca-NS" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "systeme-design.alpha.canada.ca"
-  type    = "CNAME"
+  type    = "NS"
   records = [
-    "cds-design-snc.netlify.app"
+    "ns-785.awsdns-34.net",
+    "ns-1850.awsdns-39.co.uk",
+    "ns-316.awsdns-39.com",
+    "ns-1302.awsdns-34.org",
   ]
   ttl = "300"
 }
