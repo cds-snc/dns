2ede7a56a28b44e692ed36ffa56dc3c045083096
Daine Trinidad <daine.trinidad@gmail.com>
2024-01-15 07:30:26 -0800

    chore: retire unused design.alpha.canada.ca domain (#360)

--- terraform/design.canada.ca.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route53_record" "design-alpha-canada-ca-CNAME" {
-  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-  name    = "design.alpha.canada.ca"
-  type    = "CNAME"
-  records = [
-    "gallant-bassi-851d24.netlify.app"
-  ]
-  ttl = "300"
-}
