3d7c035f1a3624d1bf6257289fa8384ae68e3768
Pat Heard <patrick.heard@cds-snc.ca>
2024-02-12 09:37:13 -0500

    chore: remove alpha DNS records (#366)
    Remove alpha DNS records that are no longer used.

--- terraform/drupal-pilot.cer.alpha.canada.ca.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route53_record" "drupal-pilot-cer-alpha-canada-ca-A" {
-  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-  name    = "drupal-pilot.cer.alpha.canada.ca"
-  type    = "A"
-  records = [
-    "52.237.15.42"
-  ]
-  ttl = "300"
-}
