51aa4f5a8615ba0a97d83b802b143a643e730eaa
William Hearn <william.hearn@statcan.gc.ca>
2024-04-25 15:38:23 +0000

    feat(dns): Add CNAME for census.alpha.canada.ca

--- terraform/statcan.alpha.canada.ca.tf
+++ terraform/statcan.alpha.canada.ca.tf
@@ -42,3 +42,43 @@ resource "aws_route53_record" "_acme-challenge-information-energie-alpha-canada-
   ]
   ttl = "300"
 }
+
+resource "aws_route53_record" "census-alpha-canada-ca-CNAME" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "census.alpha.canada.ca"
+  type    = "CNAME"
+  records = [
+    "census-cws.prod.cloud.statcan.ca"
+  ]
+  ttl = "300"
+}
+
+resource "aws_route53_record" "_acme-challenge-census-alpha-canada-ca-CNAME" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = join(".", [local.acme_challenge, aws_route53_record.census-alpha-canada-ca-CNAME.name])
+  type    = "CNAME"
+  records = [
+    join(".", [aws_route53_record.census-alpha-canada-ca-CNAME.name, local.challenges_subdomain])
+  ]
+  ttl = "300"
+}
+
+resource "aws_route53_record" "recensement-alpha-canada-ca-CNAME" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "recensement.alpha.canada.ca"
+  type    = "CNAME"
+  records = [
+    "census-cws.prod.cloud.statcan.ca"
+  ]
+  ttl = "300"
+}
+
+resource "aws_route53_record" "_acme-challenge-recensement-alpha-canada-ca-CNAME" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = join(".", [local.acme_challenge, aws_route53_record.recensement-alpha-canada-ca-CNAME.name])
+  type    = "CNAME"
+  records = [
+    join(".", [aws_route53_record.recensement-alpha-canada-ca-CNAME.name, local.challenges_subdomain])
+  ]
+  ttl = "300"
+}
