2bea930eb953570914bef9b55dd080ce94c85b0a
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-07-11 10:34:53 -0700

    Adding a new iam role that will allow notify to manage notifcation.canada.ca domain (#397)
    * Adding a new iam role that will allow notify to manage their notification.canada.ca domain
    
    * Adding GetHostedZone permission as well
    
    * Adding notify to the name so that it is more clearer
    
    * Adding notify to the name so that it is more clearer - adding a fix

---
+++ terraform/notification.canada.ca-role.tf
@@ -0,0 +1,54 @@
+# Define IAM role A with a trust policy that allows it to be assumed by terraform apply oidc role
+resource "aws_iam_role" "notify_prod_dns_manager" {
+  name = "notify_prod_dns_manager"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::296255494825:role/notification-terraform-apply"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Define the IAM policy
+resource "aws_iam_policy" "notify_prod_dns_manager_policy" {
+  name        = "notify_prod_dns_manager_policy"
+  description = "Policy to manage Route53 records for notification.canada.ca hosted zone"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "route53:ListResourceRecordSets",
+          "route53:ChangeResourceRecordSets",
+          "route53:GetChange"
+        ],
+        Effect   = "Allow",
+        Resource = "arn:aws:route53:::hostedzone/Z1XG153PQF3VV5"
+      },
+      {
+        Action = [
+          "route53:GetHostedZone",
+          "route53:ListHostedZones",
+          "route53:GetHostedZoneCount",
+          "route53:ListHostedZonesByName"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the policy to the IAM role
+resource "aws_iam_role_policy_attachment" "prod_dns_manager_policy_attachment" {
+  role       = aws_iam_role.notify_prod_dns_manager.name
+  policy_arn = aws_iam_policy.notify_prod_dns_manager_policy.arn
+}
