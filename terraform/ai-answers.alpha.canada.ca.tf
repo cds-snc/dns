0771bd9b3b8fa19f861e56d5dd330385db78aa3a
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2025-04-10 09:28:15 -0700

    Adding ai-answers

---
+++ terraform/ai-answers.alpha.canada.ca.tf
@@ -0,0 +1,12 @@
+resource "aws_route53_record" "ai-answers-alpha-canada-ca-NS" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "ai-answers.alpha.canada.ca"
+  type    = "NS"
+  records = [
+    "ns-340.awsdns-42.com.",
+    "ns-1303.awsdns-34.org.",
+    "ns-1561.awsdns-03.co.uk.",
+    "ns-981.awsdns-58.net."
+  ]
+  ttl = "300"
+}
