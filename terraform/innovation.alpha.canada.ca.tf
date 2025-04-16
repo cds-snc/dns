e3db5b84829cbdd9db92d7898e010488d37ae4d7
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2025-04-16 13:44:01 -0700

    Adding innovation.alpha.canada.ca TF record (#463)
    * Adding innovation.alpha.canada.ca TF record
    
    * Formatting

---
+++ terraform/innovation.alpha.canada.ca.tf
@@ -0,0 +1,12 @@
+resource "aws_route53_record" "innovation-alpha-canada-ca-NS" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "innovation.alpha.canada.ca"
+  type    = "NS"
+  records = [
+    "ns1-04.azure-dns.com.",
+    "ns2-04.azure-dns.net.",
+    "ns3-04.azure-dns.org.",
+    "ns4-04.azure-dns.info."
+  ]
+  ttl = "300"
+}
