899fb23eab147a9cdac2995ff3d27ccd4f9b3f92
Hamza Aburaneh <32944558+HamzaAburaneh@users.noreply.github.com>
2025-07-11 14:15:50 +0000

    chore: add TXT record for Google site verification of ai-answers.alpha.canada.ca

--- terraform/ai-answers.alpha.canada.ca.tf
+++ terraform/ai-answers.alpha.canada.ca.tf
@@ -20,3 +20,13 @@ resource "aws_route53_record" "reponses-ia-alpha-canada-ca-CNAME" {
     "ai-answers.alpha.canada.ca."
   ]
 }
+
+resource "aws_route53_record" "ai-answers-alpha-canada-ca-TXT" {
+  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+  name    = "ai-answers.alpha.canada.ca"
+  type    = "TXT"
+  records = [
+    "google-site-verification=xaKfN5L_JDtfvYZawdt_yR9D5MuzTbpg5DYat0M6LEU"
+  ]
+  ttl = "3600"
+}
