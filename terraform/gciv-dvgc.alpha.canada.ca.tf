9327b98d46eda5f3dc4ec448e5bc2a23dc7bf9e0
Eric Ehret <1770106+eehret@users.noreply.github.com>
2025-08-07 10:04:55 -0400

    Delegation of gciv-dvgc.alpha.canada.ca to Azure DNS

--- terraform/gciv-dvgc.alpha.canada.ca.tf
+++ terraform/gciv-dvgc.alpha.canada.ca.tf
@@ -3,10 +3,10 @@ resource "aws_route53_record" "gciv-dvgc-alpha-canada-ca-NS" {
   name    = "gciv-dvgc.alpha.canada.ca"
   type    = "NS"
   records = [
-    "ns-324.awsdns-40.com",
-    "ns-1309.awsdns-35.org",
-    "ns-796.awsdns-35.net",
-    "ns-1876.awsdns-42.co.uk"
+    "ns1-08.azure-dns.com",
+    "ns2-08.azure-dns.net",
+    "ns3-08.azure-dns.org",
+    "ns4-08.azure-dns.info"
   ]
   ttl = "300"
 }
