a07bcf80700fb117666c35b3ffd33b0304655d08
Ryan Hyma <Ryan.Hyma@gmail.com>
2025-08-13 15:02:53 -0400

    fix: change reponses-ia.alpha.canada.ca record type from CNAME to NS

--- terraform/ai-answers.alpha.canada.ca.tf
+++ terraform/ai-answers.alpha.canada.ca.tf
@@ -11,13 +11,16 @@ resource "aws_route53_record" "ai-answers-alpha-canada-ca-NS" {
   ttl = "300"
 }
 
-resource "aws_route53_record" "reponses-ia-alpha-canada-ca-CNAME" {
+resource "aws_route53_record" "reponses-ia-alpha-canada-ca-NS" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "reponses-ia.alpha.canada.ca"
-  type    = "CNAME"
-  ttl     = "300"
+  type    = "NS"
+  ttl     = 300
   records = [
-    "ai-answers.alpha.canada.ca."
+    "ns-340.awsdns-42.com.",
+    "ns-1303.awsdns-34.org.",
+    "ns-1561.awsdns-03.co.uk.",
+    "ns-981.awsdns-58.net."
   ]
 }
 
