b0b0f18150ee85015c88a8b615072839b5ce5e19
John Bain <john.bain@gmail.com>
2025-09-02 16:22:39 -0400

    initial

--- terraform/sp.ssc.alpha.canada.ca.tf
+++ terraform/sp.ssc.alpha.canada.ca.tf
@@ -1,5 +1,5 @@
-resource "aws_route53_record" "sp-ssc-alpha-canada-ca-NS" {
-  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
+resource "aws_route53_record" "sp_ssc_alpha_canada_ca_NS" {
+  zone_id = aws_route53_zone.alpha_canada_ca_public.zone_id
   name    = "sp.ssc.alpha.canada.ca"
   type    = "NS"
   records = [
@@ -9,4 +9,12 @@ resource "aws_route53_record" "sp-ssc-alpha-canada-ca-NS" {
     "ns4-35.azure-dns.info"
   ]
   ttl = "300"
+}
+
+resource "terraform_data" "meta_sp_ssc_alpha_canada_ca_NS" {
+  input = {
+    resource = aws_route53_record.sp_ssc_alpha_canada_ca_NS.name
+    owner    = "azurescience-scienceazure@ssc-spc.gc.ca"
+    purpose  = "Nameservers managed by the SSC Science Program"
+  }
 }
