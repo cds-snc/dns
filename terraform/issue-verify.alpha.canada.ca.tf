a00eb2a12e866f0d461739e4be35e28788b8e9a6
Eric Ehret <1770106+eehret@users.noreply.github.com>
2025-09-23 13:44:32 -0400

    Correcting a typo in the French zone for GC Issue & Verify (#496)

--- terraform/issue-verify.alpha.canada.ca.tf
+++ terraform/issue-verify.alpha.canada.ca.tf
@@ -11,15 +11,15 @@ resource "aws_route53_record" "issue-verify-alpha-canada-ca-NS" {
   ttl = "300"
 }
 
-resource "aws_route53_record" "deliverance-verification-alpha-canada-ca-NS" {
+resource "aws_route53_record" "delivrance-verification-alpha-canada-ca-NS" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-  name    = "deliverance-verification.alpha.canada.ca"
+  name    = "delivrance-verification.alpha.canada.ca"
   type    = "NS"
   records = [
-    "ns1-08.azure-dns.com",
-    "ns1-08.azure-dns.com",
-    "ns3-08.azure-dns.org",
-    "ns4-08.azure-dns.info"
+    "ns1-07.azure-dns.com",
+    "ns2-07.azure-dns.net",
+    "ns3-07.azure-dns.org",
+    "ns4-07.azure-dns.info"
   ]
   ttl = "300"
 }
