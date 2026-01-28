279483777856b16b4d4834cc5fc4eb60fe27451b
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2026-01-28 15:06:36 -0500

    feat: setup_slack_domain_verif (#517)

--- terraform/cds-snc.ca.tf
+++ terraform/cds-snc.ca.tf
@@ -190,7 +190,8 @@ resource "aws_route53_record" "cds-snc-ca-TXT" {
     "1password-site-verification=SQMY32ZPXJAUDLOODGRBDPPR5Q",
     "figma-domain-verification=16d1dad1fe0cfd4f60d293c59bea524ddf20d5e4371d5109a6871071d63421f4-1757620344",
     "zapier-domain-verification-challenge=eb3188eb-7860-4405-9f83-8535eec1de68",
-    "druide-validation-domaine=5e7552be9d91c4d8fd52d80df96c8ea8"
+    "druide-validation-domaine=5e7552be9d91c4d8fd52d80df96c8ea8",
+    "slack-domain-verification=e7oExPI0DmmF5kjd0RebirKQWGCEuxA74PqMODUE"
   ]
   ttl = "3600"
 
