b5774d4b31acbc6897df4e9fcb0cc0d5f8c00720
Pat Heard <patrick.heard@cds-snc.ca>
2026-03-02 08:41:16 -0500

    chore: remove NS for ai-answers

--- terraform/ai-answers.alpha.canada.ca.tf
+++ terraform/ai-answers.alpha.canada.ca.tf
@@ -1,29 +1,3 @@
-resource "aws_route53_record" "ai-answers-alpha-canada-ca-NS" {
-  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-  name    = "ai-answers.alpha.canada.ca"
-  type    = "NS"
-  records = [
-    "ns-340.awsdns-42.com.",
-    "ns-1303.awsdns-34.org.",
-    "ns-1561.awsdns-03.co.uk.",
-    "ns-981.awsdns-58.net."
-  ]
-  ttl = "300"
-}
-
-resource "aws_route53_record" "reponses-ia-alpha-canada-ca-NS" {
-  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-  name    = "reponses-ia.alpha.canada.ca"
-  type    = "NS"
-  ttl     = 300
-  records = [
-    "ns-198.awsdns-24.com.",
-    "ns-1738.awsdns-25.co.uk.",
-    "ns-647.awsdns-16.net.",
-    "ns-1448.awsdns-53.org."
-  ]
-}
-
 resource "aws_route53_record" "ai-answers-alpha-canada-ca-TXT" {
   zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
   name    = "ai-answers.alpha.canada.ca"
