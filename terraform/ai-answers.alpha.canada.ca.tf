15804470d079ff69495c1218c35c152e91847fe3
Hamza Aburaneh <32944558+HamzaAburaneh@users.noreply.github.com>
2026-03-02 11:09:28 -0500

    chore: remove google TXT record. (#533)

--- terraform/ai-answers.alpha.canada.ca.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route53_record" "ai-answers-alpha-canada-ca-TXT" {
-  zone_id = aws_route53_zone.alpha-canada-ca-public.zone_id
-  name    = "ai-answers.alpha.canada.ca"
-  type    = "TXT"
-  records = [
-    "google-site-verification=xaKfN5L_JDtfvYZawdt_yR9D5MuzTbpg5DYat0M6LEU"
-  ]
-  ttl = "3600"
-}
