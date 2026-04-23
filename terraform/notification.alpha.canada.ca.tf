2b0cf69aebb4c8d3f01dfd9029a4bc0606201ee8
Ben Larabie <benlarabie@gmail.com>
2026-04-23 13:11:49 -0400

    Revert "move alpha to k8s (#540)"
    This reverts commit 6e576c24b70accc9ef2d65955f29dbd58f465d51.

--- terraform/notification.alpha.canada.ca.tf
+++ terraform/notification.alpha.canada.ca.tf
@@ -1,7 +1,10 @@
 locals {
-  notification_alb             = "notification-production-alb-1685085140.ca-central-1.elb.amazonaws.com"
-  notification_zone_id         = "ZQSVJUPU6J1EY"
-  api_gateway_regional_zone_id = "Z19DQILCV0OWEC" # ca-central-1
+  notification_alb                              = "notification-production-alb-1685085140.ca-central-1.elb.amazonaws.com"
+  notification_zone_id                          = "ZQSVJUPU6J1EY"
+  api_gateway_regional_zone_id                  = "Z19DQILCV0OWEC" # ca-central-1
+  api_lambda_gateway_domain_name_api_lambda     = "d-0jho4qbdqi.execute-api.ca-central-1.amazonaws.com"
+  api_lambda_gateway_domain_name_alt_api_lambda = "d-f9v7fu3260.execute-api.ca-central-1.amazonaws.com"
+  api_lambda_gateway_domain_name_api            = "d-jwtzdgd9qg.execute-api.ca-central-1.amazonaws.com"
 }
 
 resource "aws_route53_record" "notification-alpha-canada-ca-ALIAS" {
@@ -22,8 +25,8 @@ resource "aws_route53_record" "api-notification-alpha-canada-ca-A" {
   type    = "A"
 
   alias {
-    name                   = local.notification_alb
-    zone_id                = local.notification_zone_id
+    name                   = local.api_lambda_gateway_domain_name_alt_api_lambda
+    zone_id                = local.api_gateway_regional_zone_id
     evaluate_target_health = true
   }
 }
